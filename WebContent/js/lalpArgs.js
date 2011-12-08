605749d4a4ab4d0e29c08ba3970c10d6c79314d6
gabrieljt@gmail.com <gabrieljt@gmail.com@08462526-9d5c-11de-b798-49b661d4457b>
2011-12-08 08:30:08 +0000

    Server path for writing compilation files is now dynamic (DEFAULT_DIRECTORY in Graphiz.java & VHDL.java) Parameters menu removed, only the working configuration is now executed SVG files created from compilation DOT files in LalpServlet GET method Reads generated VHD code and prints on 'Target Code' div Loads SVG files via AJAX JQuery SVG lib added

--- WebContent/js/lalpArgs.js
+++ WebContent/js/lalpArgs.js
@@ -1,27 +1,20 @@
 /* global vars */
 var selected = false;
-var graphviz = false;
 var args = new Array();
-$('#graphMenu').hide();
 
-// parameters
-var menuArgs = $('form input:radio[name=menu]');
-menuArgs.click(function() { // select radio list option
-	args[0] = $(this).val();
-	if (args[0] == "-gv") {
-		graphviz = true;		
-		$('#graphType').val($('form input:radio[name=subMenu]').val());
-		$('#graphMenu').show();
-	} else {
-		graphviz = false;
-		$('#graphMenu').hide();
-	}		
-});
-
-var graphMenuArgs = $('form input:radio[name=subMenu]');
-graphMenuArgs.click(function() { // select radio list option	
-	$('#graphType').val($(this).val());
-});
+// user args not working yet
+/*
+ * $('#graphMenu').hide(); var menuArgs = $('form input:radio[name=menu]');
+ * menuArgs.click(function() { // select radio list option args[0] =
+ * $(this).val(); if (args[0] == "-gv") { graphviz = true;
+ * $('#graphType').val($('form input:radio[name=subMenu]').val());
+ * $('#graphMenu').show(); } else { graphviz = false; $('#graphMenu').hide(); }
+ * });
+ * 
+ * var graphMenuArgs = $('form input:radio[name=subMenu]');
+ * graphMenuArgs.click(function() { // select radio list option
+ * $('#graphType').val($(this).val()); });
+ */
 
 // download source code
 $('#download').click(function() {
@@ -33,6 +26,7 @@ $('#targetDownload').click(function() {
 	$('#targetDownloadForm').submit();
 });
 
+// upload .alp file
 $('#upload').click(function() {
 	$.ajaxFileUpload({
 		url : 'UploadServlet',
@@ -45,10 +39,8 @@ $('#upload').click(function() {
 			data = data.replace("</pre>", "");
 			data = data.trim();
 			$('#sourceCodeArea').html(data);
-			$('#graphCode').html(data);
 			fileName = $('form input:file').val();
 			$('#fileName').val(fileName);
-			$('#graphFileName').val(fileName);
 			$('#targetCodeArea').html('');
 		},
 		error : function(data, status, e) {
@@ -58,28 +50,45 @@ $('#upload').click(function() {
 });
 
 $('#compile').attr('disabled', false);
-$('#compile').click(
-		function() {
-			if (graphviz) {			
-				$('#graphForm').submit();							
-			} else {
-				$.ajax({
-					url : 'LALPServlet',
-					type : 'POST',
-					data : {
-						'args[]' : args,
-						fileName : $('#fileName').val(),
-						sourceCode : $('#sourceCodeArea').val(),
-					},
-					error : function() {
-						alert('AJAX: Response from server failed!');
-					},
-					success : function(data) {
-						$('#targetCodeArea').html(data);
-						$('#targetFileName').val(
-								$('#fileName').val().replace(".alp", ".vhd"));
-					}
-				});
-			}
+// compiles once and generate all files; SVG files are created from dot files
+$('#compile').click(function() {
+	requestVHD();
+	setTimeout(function() {
+		requestSVG('sw');
+		requestSVG('hw');
+	}, 5000); // waits 5sec after compilation to create SVG files (assures that .dot file was already created)	
+});
 
-		});
+/* AJAX & SVG */
+function requestVHD() {
+	$.ajax({
+		url : 'LALPServlet',
+		type : 'POST',
+		data : {
+			'args[]' : args,
+			fileName : $('#fileName').val(),
+			sourceCode : $('#sourceCodeArea').val(),
+		},
+		error : function() {
+			alert('AJAX: Response from server failed!');
+		},
+		success : function(data) {
+			$('#targetCodeArea').html(data);
+			$('#targetFileName').val(
+					$('#fileName').val().replace(".alp", ".vhd"));
+		}
+	});
+}
+
+function requestSVG(graphType) {
+	args = null; // not working yet
+	sourceCode = $('#sourceCodeArea').val();
+	fileName = $('#fileName').val();
+	if (graphType == 'sw') {
+		$('#swSVG').svg();
+		$('#swSVG').load('LALPServlet?&fileName=' + fileName + '&graphType=' + graphType);
+	} else if (graphType == 'hw') {
+		$('#hwSVG').svg();
+		$('#hwSVG').load('LALPServlet?&fileName=' + fileName + '&graphType=' + graphType);
+	}
+}
