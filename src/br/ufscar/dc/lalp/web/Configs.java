166cd2788c2384426d72308f402f09d11cc0347d
Ricardo Menotti <menotti@users.noreply.github.com>
2013-03-12 13:22:25 +0000

    Minor changes in LALPG grammar and generation

--- src/br/ufscar/dc/lalp/web/Configs.java
+++ src/br/ufscar/dc/lalp/web/Configs.java
@@ -3,28 +3,33 @@ package br.ufscar.dc.lalp.web;
 public class Configs {
 	
 	//senha do email lalp.ufscar@gmail.com
-	private static String EMAIL_PASS = "************";
+	private static final String EMAIL_PASS = "************";
 	
 	//senha do mysql no servidor lalp.dc.ufscar.br
-	private static String MYSQL_PASS = "************";
+	private static final String MYSQL_USER = "lalp";  
+	private static final String MYSQL_PASS = "************";
 	
 	//caminho dos arquivos de compilacao
-	public static final String COMP_FILES_PATH = "/usr/share/tomcat7/webapps/lalp/"; //server
+	public static final String COMP_FILES_PATH = "/home/profs/menotti/tomcat/webapps/lalp/"; //server
+	//public static final String COMP_FILES_PATH = "/usr/share/tomcat7/webapps/lalp/"; //server
 	//public static final String COMP_FILES_PATH = "/Users/menotti/Documents/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/lalp/"; //menotti
 	//private static String COMP_FILES_PATH = "C:/Users/Tulio/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/lalp/"; //tulio
 	
 	//caminho dos exemplos
-	private static final String EXAMPLES_PATH = "/usr/share/tomcat7/webapps/lalp/examples/"; //server
+	private static final String EXAMPLES_PATH = "/home/profs/menotti/tomcat/webapps/lalp/examples/"; //server
+	//private static final String EXAMPLES_PATH = "/usr/share/tomcat7/webapps/lalp/examples/"; //server
 	//private static final String EXAMPLES_PATH = "/Users/menotti/Documents/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/lalp/examples/"; //menotti
 	//private static final String EXAMPLES_PATH = "C:/Users/Tulio/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/lalp/examples/"; //tulio
 	
 	//caminho do zip
-	private static final String ZIP_PATH = "/usr/share/tomcat7/webapps/lalp/"; //server
+	private static final String ZIP_PATH = "/home/profs/menotti/tomcat/webapps/lalp/"; //server
+	//private static final String ZIP_PATH = "/usr/share/tomcat7/webapps/lalp/"; //server
 	//private static final String ZIP_PATH = "/Users/menotti/Documents/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/lalp/examples/"; //menotti
 	//private static final String ZIP_PATH = "C:/Users/Tulio/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/lalp/"; //tulio
 	
 	//caminho do server
-	private static final String SERVER_PATH= "http://lalp.dc.ufscar.br:8080/lalp/"; //server
+	private static final String SERVER_PATH= "http://projetos.ppgcc.dc.ufscar.br:9180/lalp/"; //server
+	//private static final String SERVER_PATH= "http://lalp.dc.ufscar.br:8080/lalp/"; //server
 	//private static final String SERVER_PATH = "http://localhost:8080/lalp/"; //local
 	
 	//caminho do Graphviz
@@ -36,6 +41,10 @@ public class Configs {
 		return EMAIL_PASS;
 	}
 
+	public static String getMysqlUser() {
+		return MYSQL_USER;
+	}
+	
 	public static String getMYSQL_PASS() {
 		return MYSQL_PASS;
 	}
