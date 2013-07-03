83863c5f6d772c288d1cc50425ab6ce54d9501ea
lucasfporto@gmail.com <lucasfporto@gmail.com@08462526-9d5c-11de-b798-49b661d4457b>
2013-07-03 18:08:48 +0000

    Correcao de erro durante o processo de geracao de nome dos componentes de operacao.

--- Main.cpp
+++ Main.cpp
@@ -394,7 +394,7 @@ int main(int argc, char * argv[]){
     listaCom.identificaFor();
     //listaCom.geraGrafo();
     listaCom.FinalizaComponentes();
-    //listaCom.imprimeAll();
+    listaCom.imprimeAll();
     //listaCom.imprimeTodosComponentes();
     cout<<"###############################################"<<endl;
     
