109c1c16402a4083f88db1ebd12eb41921101cca
lucasfporto@gmail.com <lucasfporto@gmail.com@08462526-9d5c-11de-b798-49b661d4457b>
2013-09-03 13:31:29 +0000

    Correção

--- branches/LALPC/Componente/op_add_s.cpp
+++ branches/LALPC/Componente/op_add_s.cpp
@@ -8,7 +8,7 @@
 #include "op_add_s.h"
 
 op_add_s::op_add_s(SgNode* node) : op_simple(node){
-    this->setNomeCompVHDL("op_add_s");
+    this->setNomeCompVHDL("add_op_s");
     this->op_tipo       = "ADD";
     this->setSync(false);
 }
