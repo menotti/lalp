1ef372ea7d4284489c875f994c6d16a171d49f0f
lucasfporto@gmail.com <lucasfporto@gmail.com@08462526-9d5c-11de-b798-49b661d4457b>
2014-06-23 16:17:14 +0000

    Exemplo accumulation e correcao de geracao do sinal result de maneira errada

---
+++ Exemplos/accumulation/accumulation.c
@@ -0,0 +1,15 @@
+
+// Simple accumulation
+int main() {
+
+int a[10] = {1,2,3,4,5,6,7,8,9,10};
+int sum = 0;
+int i;
+for (i = 0; i < 10; i++) {
+	sum += a[i] + i;
+	//printf("sum %d = %d\n", i, sum);
+}
+//printf("sum = %d\n", sum);
+
+return sum;
+}
