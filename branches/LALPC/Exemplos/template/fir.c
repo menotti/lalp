402a1fa08de6ab1eebe73c3b55f4eecad4edc5c3
lucasfporto@gmail.com <lucasfporto@gmail.com@08462526-9d5c-11de-b798-49b661d4457b>
2015-03-20 19:57:42 +0000

    Corrigindo arquivos em C dos exemplos: fir

--- branches/LALPC/Exemplos/template/fir.c
+++ branches/LALPC/Exemplos/template/fir.c
@@ -4,12 +4,12 @@ int main() {
   int c[4] = {1, 2, 3, 4};
   int d[4] = {1, 2, 3, 4};
   int e[4] = {1, 2, 3, 4};
-  int sum = 0;
+  int sum[4];
   int i = 0;
   #pragma out sum
-  #pragma unroll 2
-  #pragma multport
+  //#pragma unroll 2
+  //#pragma multport
   for(i = 0; i < 4; i++){
-	sum = (((a[i] * 3)+(b[i] * 5))+(c[i] * 7))+((d[i] * 9)+(e[i] * 11));
+	sum[i] = (((a[i] * 3)+(b[i] * 5))+(c[i] * 7))+((d[i] * 9)+(e[i] * 11));
   }
 }
